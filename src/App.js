96a2ef94fbfc5684e6f1a861a42ce8445334c303
Svitlana Rybakova <rsv0011@gmail.com>
2021-02-11 15:55:45 +0100

    add useState and uuid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SongList from './components/SongList'
+
 function App() {
+ 
   return (
     <div className="App">
       <SongList />
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function SongList() {
+  const [songs, setSong] = useState([
+    { title: 'Bruno Mars "Treasure"', id: 1 },
+    { title: 'Jason Derulo "Take you dancing"', id: 2 },
+    { title: 'Madonna', id: 3 },
+  ]);
+  
+  function addSong(){
+    setSong([...songs, { title: 'new song', id: uuidv4()}])
+  }
+  
+  console.log(songs);
+  
   return(
     <div className="song-list">
       <ul>
-        <li>Bruno Mars "Treasure"</li>
-        <li>Jason Derulo "Take you dancing"</li>
+        {songs.map(song => {
+          return(
+            <li key={song.id}>{song.title}</li>
+          )})
+        }
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   )
 }
