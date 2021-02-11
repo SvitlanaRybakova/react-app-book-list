653f4ccc44e6b9c7b0278f7c421cc70035e8329f
Svitlana Rybakova <rsv0011@gmail.com>
2021-02-11 15:26:39 +0100

    init project

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SongList() {
+  return(
+    <div className="song-list">
+      <ul>
+        <li>Bruno Mars "Treasure"</li>
+        <li>Jason Derulo "Take you dancing"</li>
+      </ul>
+    </div>
+  )
+}
+export default SongList;
